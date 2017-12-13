4484cb692cb8d8db9fb93be8adcb94081e809919
alejandroquintero <a.quintero10@uniandes.edu.co>
2017-12-13 09:33:17 -0500

    nueva libreria

--- auth0_utils/src/main/java/co/edu/uniandes/csw/auth/service/AuthService.java
+++ auth0_utils/src/main/java/co/edu/uniandes/csw/auth/service/AuthService.java
@@ -24,16 +24,9 @@ SOFTWARE.
  */
 package co.edu.uniandes.csw.auth.service;
 
-import co.edu.uniandes.csw.auth.conexions.AuthenticationApi;
-import co.edu.uniandes.csw.auth.conexions.AuthorizationApi;
 import co.edu.uniandes.csw.auth.model.UserDTO;
-import co.edu.uniandes.csw.auth.conexions.CacheManager;
 import co.edu.uniandes.csw.auth.filter.StatusCreated;
-import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import io.jsonwebtoken.Claims;
-import io.jsonwebtoken.Jws;
-import java.util.Iterator;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.ws.rs.Consumes;
@@ -44,11 +37,10 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import org.json.JSONException;
-import org.json.JSONObject;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
+import java.util.logging.Logger;
 
 
 @Path("/users")
@@ -62,52 +54,25 @@ public class AuthService {
     @Context
     private HttpServletRequest req;
 
-    private final AuthenticationApi auth;
-    private final AuthorizationApi authorization;
-    private static boolean logged = false;
     
-    public AuthService() throws IOException, UnirestException, JSONException, InterruptedException, ExecutionException {
-        this.auth = new AuthenticationApi();
-        this.authorization = new AuthorizationApi();
-        
-     
-    }
 
     @Path("/login")
     @POST
     public UserDTO login(UserDTO user) throws UnirestException, JSONException, IOException, InterruptedException, ExecutionException {
-        String str;
-        Integer code = auth.managementUpdateClientGrants().getCode();
-        if (code == 200) {
-            str = auth.getSubject(user, rsp);
-            user.setRoles(CacheManager.getRolesByUserCache().get(str));
-            user.setPermissions(CacheManager.getPermissionsCache().get(str));
-            logged = true;
-        }
-        return user;
+        return null;
     }
 
     @Path("/logout")
     @GET
     public void logout() {
-        auth.authenticationLogout();
+        
     }
 
     @Path("/register")
     @POST
     @StatusCreated
     public void register(UserDTO user) throws UnirestException, JSONException, IOException, InterruptedException, ExecutionException {
-        HttpResponse<String> rs = auth.authenticationSignUP(user);
-        JSONObject json = new JSONObject(rs.getBody());
-        String str = (String) json.get("_id");
-        authorization.authorizationAddUserToGroup(str);
-        HttpResponse<String> response = CacheManager.getRolesCache().get("roles");
-        List<String> list = authorization.getSignUpRolesId(user, response);
-        Iterator<String> it = list.iterator();
-        while (it.hasNext()) {
-            authorization.authorizationAddRoleToUser(str, it.next());
-        }
-        auth.HttpServletResponseBinder(rs, rsp);
+       System.out.println(user);
     }
 
     @Path("/me")
@@ -115,27 +80,8 @@ public class AuthService {
     public UserDTO getCurrentUser() throws JSONException, UnirestException, IOException, InterruptedException, ExecutionException {
        
         
-        
-        Jws<Claims> claim = null;
-        if (logged & req.getCookies()!=null) {
-            claim = auth.decryptToken(req);
-        }
-        String subject;
-        if (claim != null) {
-            subject = claim.getBody().getSubject();           
-            UserDTO user = CacheManager.getProfileCache().get(subject);
-            user.setRoles(CacheManager.getRolesByUserCache().get(subject));
-            user.setPermissions(CacheManager.getPermissionsCache().get(subject));
-            return user;
-        }
-        if("development".equals(this.auth.getProp().getProperty("environment"))){
-        UserDTO user = CacheManager.getProfileCache().get("auth");
-            user.setRoles(CacheManager.getRolesByUserCache().get("auth"));
-            user.setPermissions(CacheManager.getPermissionsCache().get("auth"));
-            return user;
-        }
-            
-        return null;
+      return null;  
+      
     }
 
 }
